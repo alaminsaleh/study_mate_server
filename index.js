a83a04b6ee8db99808e3016b95b803f0a47be957
alaminsaleh <abusalehalamin52@gmail.com>
2025-12-06 12:54:27 +0600

    Add route to fetch a single partner by ID

--- index.js
+++ index.js
@@ -71,7 +71,15 @@ async function run() {
             res.send(result);
         })
 
-        
+        app.get('/partners/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await partnerCollection.findOne(query);
+            res.send(result);
+        })
+
+
+
 
 
 
